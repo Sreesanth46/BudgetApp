e19063b4e93310eb2f68409a20f65e50b0a0bec6
Sreesanth46 <sreesanth46@gmail.com>
2023-06-08 19:36:13 +0530

    feat: groups model

---
+++ models/Groups.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+    const Groups = sequelize.define("groupMaster", {
+        name: {
+            type: DataTypes.STRING(100),
+        },
+        status: {
+            type: DataTypes.TINYINT,
+            allowNull: false,
+            defaultValue: 1,
+            validate: {
+                notEmpty: true
+            }
+        },
+    })
+
+    Groups.associate = (models) => {
+        Groups.hasMany(models.groupMembers, {
+            foreignKey: 'groupId',
+            as: 'groupMembers',
+            onDelete: "cascade"
+        });
+    };
+
+    return Groups
+}
