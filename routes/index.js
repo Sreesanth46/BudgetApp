9ae344c01f74e2cda6941ec63af979ffa309fbc6
Sreesanth46 <sreesanth46@gmail.com>
2023-06-08 21:27:17 +0530

    feat: login route

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ const router = require('express').Router();
 
 router.use('/register', require('./api/registeration.js'))
 
+router.use('/login', require('./api/login.js'))
+
 module.exports = router
