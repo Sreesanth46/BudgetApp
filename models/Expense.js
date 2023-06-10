ca4d6c7ddca592bd51d1c3035ef176eeb17f2e51
Sreesanth46 <sreesanth46@gmail.com>
2023-06-10 21:16:21 +0530

    feat: expense Model

---
+++ models/Expense.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+    const Expense = sequelize.define("expenseMaster", {
+        name: {
+            type: DataTypes.STRING(100),
+        },
+        amount: {
+            type: DataTypes.INTEGER,
+        },
+        status: {
+            type: DataTypes.TINYINT,
+            allowNull: false,
+            defaultValue: 1,
+            validate: {
+                notEmpty: true
+            }
+        },
+    })
+
+    Expense.associate = (models) => {
+        Expense.belongsTo(models.groupMembers, {
+            foreignKey: 'createdBy',
+            as: 'user',
+            onDelete: "cascade"
+        });
+
+        Expense.belongsTo(models.groupMaster, {
+            foreignKey: 'groupId',
+            as: 'group',
+            onDelete: "cascade"
+        });
+
+        Expense.hasMany(models.expenseSplit, {
+            foreignKey: 'expenseId',
+            as: 'expense',
+            onDelete: "cascade"
+        });
+    };
+
+    return Expense
+}
