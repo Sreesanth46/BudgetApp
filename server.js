c5274c37637ba6454a032b9e47f876592bfd7efd
Sreesanth46 <sreesanth46@gmail.com>
2023-06-12 10:08:50 +0530

    fix: env config

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const http = require('http');
 const app = require('./app');
 const server = http.createServer(app)
 const db = require('./models')
+require('dotenv').config()
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9080;
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
