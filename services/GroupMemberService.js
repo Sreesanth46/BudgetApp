748a88c238c5f54288b41cd23901435051942506
Sreesanth46 <sreesanth46@gmail.com>
2023-06-12 14:39:37 +0530

    feat: findById, delete, findByUserIdAndGroupId

--- services/GroupMemberService.js
+++ services/GroupMemberService.js
@@ -1,8 +1,45 @@
 const db = require('../models')
 const GroupMember = db.groupMember
+const Group = db.groupMaster
 
 exports.save = async (form) => {
     return GroupMember.create({
         ...form
+    })
+}
+
+exports.findById = async (id) => {
+    return GroupMember.findOne({
+        where: {
+            [Op.and]: [
+                { id },
+                { status: { [Op.ne]: 99 } }
+            ]
+        },
+        include: {
+            model: Group,
+            as: 'group'
+        }
+    })
+}
+
+exports.findByUserIdAndGroupId = async ({ userId, groupId }) => {
+    return GroupMember.findAll({
+        where: {
+            [Op.and]: [
+                { status: { [Op.ne]: 99 } },
+                { userId },
+                { groupId },
+            ]
+        }
+    })
+}
+
+exports.delete = async (id) => {
+    return GroupMember.update({
+        status: 99,
+        where: {
+            id
+        }
     })
 }
