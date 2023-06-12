de02e4a8d4a672523d9abc86676a9f37fce2d7f2
Sreesanth46 <sreesanth46@gmail.com>
2023-06-12 14:38:11 +0530

    feat: delete, create

--- controllers/GroupMembersController.js
+++ controllers/GroupMembersController.js
@@ -1,7 +1,20 @@
 const groupMemberService = require('../services/GroupMemberService')
+const { createError } = require('../error/error');
 
-exports.addMember = async (req, res, next) => {
+exports.create = async (req, res, next) => {
     const { userId, groupId } = req.body
-    const groupMember = await groupMemberService.save({ userId, groupId })
+    let groupMember = await groupMemberService.findByUserIdAndGroupId({ userId, groupId })
+    if (groupMember) return res.status(400).json({ message: 'Member already exists' })
+    groupMember = await groupMemberService.save({ userId, groupId })
     return res.status(201).json({ message: 'Group member added successfully', groupMember })
+}
+
+exports.delete = async (req, res, next) => {
+    const { id } = req.params
+    const { uId } = req.user
+    let groupMember = await groupMemberService.findById(id)
+    if (!groupMember) return next(createError(404, 'Member not found'))
+    if (groupMember.group.adminId !== uId) return next(createError(403, 'You are not the admin of this group'))
+    groupMember = await groupMemberService.delete(id)
+    return res.status(201).json({ message: 'Member was deleted', groupMember })
 }
