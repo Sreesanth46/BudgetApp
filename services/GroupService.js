40ccd890cd6548a144a72cca02c27adaa4d6ac61
Sreesanth46 <sreesanth46@gmail.com>
2023-06-12 10:14:43 +0530

    feat: findAllByUAdminId, update model operation

--- services/GroupService.js
+++ services/GroupService.js
@@ -1,8 +1,26 @@
 const db = require('../models')
 const Groups = db.groupMaster
+const { Op } = require('sequelize');
 
 exports.save = async (form) => {
     return Groups.create({
+        ...form
+    })
+}
+
+exports.findAllByUAdminId = async (adminId) => {
+    return Groups.findAll({
+        where: {
+            [Op.and]: [
+                { status: { [Op.ne]: 99 } },
+                { adminId },
+            ]
+        }
+    })
+}
+
+exports.update = async (form) => {
+    return Groups.update({
         ...form
     })
 }
