7ff997921736ab336da7d10a27ea87d653fc3756
Sreesanth46 <sreesanth46@gmail.com>
2023-06-12 10:09:34 +0530

    feat: edit group

--- controllers/GroupController.js
+++ controllers/GroupController.js
@@ -1,7 +1,14 @@
 const groupService = require('../services/GroupService')
 
-exports.createGroup = async (req, res, next) => {
+exports.create = async (req, res, next) => {
     const { name } = req.body
+    const { uId } = req.user
     const groups = await groupService.save({ name })
-    return res.status(200).json({ message: 'Group created successfully', groups })
+    return res.status(201).json({ message: 'Group created successfully', groups })
+}
+
+exports.update = async (req, res, next) => {
+    const { name } = req.body
+    const groups = await groupService.update({ name })
+    return res.status(201).json({ message: 'Group updated successfully', groups })
 }
