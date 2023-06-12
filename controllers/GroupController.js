1915cdc13419a72bac5eea788d9099742e1798bf
Sreesanth46 <sreesanth46@gmail.com>
2023-06-12 17:10:48 +0530

    fix: update, delete

--- controllers/GroupController.js
+++ controllers/GroupController.js
@@ -1,3 +1,4 @@
+const { createError } = require('../error/error')
 const groupService = require('../services/GroupService')
 
 exports.create = async (req, res, next) => {
@@ -9,7 +10,13 @@ exports.create = async (req, res, next) => {
 
 exports.update = async (req, res, next) => {
     const { name } = req.body
-    const groups = await groupService.update({ name })
+    const { id } = req.params
+    const { uId } = req.user
+    let groups = await groupService.findById(id)
+    if (!groups) return next(createError(404, 'Group not found'))
+    if (groups.adminId !== uId) return next(createError(404, 'You are not the admin of the group'))
+
+    groups = await groupService.update({ name, id })
     return res.status(201).json({ message: 'Group updated successfully', groups })
 }
 
@@ -17,8 +24,9 @@ exports.delete = async (req, res, next) => {
     const { id } = req.params
     const { uId } = req.user
     let groups = await groupService.findById(id)
-    if (!groups) return res.status(404).json({ message: 'Group not found' })
+    if (!groups) return next(createError(404, 'Group not found'))
     if (groups.adminId !== uId) return res.status(403).json({ message: 'You are not the admin of the group' })
+
     groups = await groupService.delete(id)
     return res.status(201).json({ message: 'Group deleted', groups })
 }
