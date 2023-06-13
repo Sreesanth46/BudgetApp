69d7bae23880f4d8210c63661462bf16cc9f5a91
Sreesanth46 <sreesanth46@gmail.com>
2023-06-13 17:00:11 +0530

    feat: search, update

---
+++ controllers/UsersController.js
@@ -0,0 +1,15 @@
+const { createError } = require("../error/error")
+const userService = require('../services/UserService')
+
+exports.update = async (req, res, next) => {
+    const { uId } = req.user
+    const { upi, phone, name, profilePic } = req.body
+    const user = await userService.update({ upi, phone, name, profilePic, id: uId })
+    return res.status(201).json(user)
+}
+
+exports.searchUsers = async (req, res, next) => {
+    const { search } = req.query
+    const users = await userService.findAllByEmailOrPhone(search)
+    return res.status(200).json(users)
+}
