34a2e6a4e999a7cbe5ab11b53e692d0bfd6fe62e
Sreesanth46 <sreesanth46@gmail.com>
2023-06-13 17:00:26 +0530

    feat: users route

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,6 @@ router.use('/register', require('./api/registeration.js'))
 
 router.use('/login', require('./api/login.js'))
 
+router.use('/users', require('./authenticated/users.js'))
+
 module.exports = router
