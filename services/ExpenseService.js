944cd7eaf164169135882de55ddb1f6a677a8b06
Sreesanth46 <sreesanth46@gmail.com>
2023-06-13 18:29:10 +0530

    feat: listByCreator

--- services/ExpenseService.js
+++ services/ExpenseService.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Expense = db.expenseMaster
-const { Op } = require('sequelize');
+const GroupMember = db.groupMembers
+const Users = db.userMaster
+const { Op } = db.Sequelize
 
 exports.save = async (form) => {
     return Expense.create({
@@ -19,30 +21,56 @@ exports.findById = async (id) => {
     })
 }
 
+exports.listByCreatedBy = async (createdBy) => {
+    return Expense.findAll({
+        where: {
+            [Op.and]: [
+                { createdBy },
+                { status: { [Op.ne]: 99 } }
+            ]
+        },
+        include: {
+            model: GroupMember,
+            as: 'createdUser',
+            include: {
+                model: Users,
+                as: 'user',
+                attributes: { exclude: ['password'] }
+            }
+        }
+    })
+}
+
 exports.update = async ({ name, amount, id }) => {
     return Expense.update({
         name,
         amount,
-        where: {
-            id
-        }
-    })
+    },
+        {
+            where: {
+                id
+            }
+        })
 }
 
 exports.markAsPaid = async (id) => {
     return Expense.update({
         status: 2,
-        where: {
-            id
-        }
-    })
+    },
+        {
+            where: {
+                id
+            }
+        })
 }
 
 exports.delete = async (id) => {
     return Expense.update({
         status: 99,
-        where: {
-            id
-        }
-    })
+    },
+        {
+            where: {
+                id
+            }
+        })
 }
