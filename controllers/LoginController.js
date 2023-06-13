8e2e9fa408148f91f4f6348c345b5cf177b74a52
Sreesanth46 <sreesanth46@gmail.com>
2023-06-13 14:02:05 +0530

    fix: createError, refreshToken-spread operator

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,20 +1,21 @@
 const userService = require('../services/UserService')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+const { createError } = require('../error/error')
 
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET || "AccessSecretKey"
-const accessTokenExpiry = process.env.ACCESS_TOKEN_EXPIRY
+const accessTokenExpiry = process.env.ACCESS_TOKEN_EXPIRY || '1d'
 const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET || "RefreshSecretKey"
-const refreshTokenExpiry = process.env.ACCESS_TOKEN_EXPIRY
+const refreshTokenExpiry = process.env.ACCESS_TOKEN_EXPIRY || '7d'
 
 exports.login = async (req, res, next) => {
     const { email, password } = req.body
     let authUser = await userService.findByEmail(email)
 
-    if (!authUser) return res.status(400).json({ message: "Invalid email" })
+    if (!authUser) return next(createError(400, 'Invalid email'))
 
     const isEqual = await bcrypt.compare(password, authUser.password)
-    if (!isEqual) return res.status(400).json({ message: "Invalid password or username" })
+    if (!isEqual) return next(createError(400, 'Invalid username or password'))
 
     try {
         const { password, ...others } = authUser.dataValues
@@ -29,18 +30,19 @@ exports.login = async (req, res, next) => {
 
 exports.verifyAccessToken = async (req, res, next) => {
     const { accessToken } = req.body
-    if (!accessToken) return res.status(400).json({ message: 'No access token' })
+    if (!accessToken) return next(createError(400, 'No access token'))
     jwt.verify(accessToken, accessTokenSecret, (err, user) => {
-        if (err) return res.status(401).json({ message: "Token is invalid" })
+        if (err) return next(createError(401, 'Token is invalid'))
         return res.status(200).json(user)
     })
 }
 
-exports.refreshAccessToken = async () => {
+exports.refreshAccessToken = async (req, res, next) => {
     const { refreshToken } = req.body
     jwt.verify(refreshToken, refreshTokenSecret, async (err, user) => {
-        if (err) return res.status(401).json({ message: "Token is invalid" })
-        const accessToken = await jwt.sign({ ...user }, accessTokenSecret, { expiresIn: `${accessTokenExpiry}` })
-        return res.status(200).json({ ...user, accessToken })
+        if (err) return next(createError(401, 'Token is invalid'))
+        const { iat, exp, ...rest } = user
+        const accessToken = await jwt.sign({ ...rest }, accessTokenSecret, { expiresIn: `${accessTokenExpiry}` })
+        return res.status(200).json({ ...rest, accessToken })
     })
 }
