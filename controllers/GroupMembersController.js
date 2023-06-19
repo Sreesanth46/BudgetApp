bc6035384507396ec4c9881910acf3891a6c1eef
Sreesanth46 <sreesanth46@gmail.com>
2023-06-19 18:09:27 +0530

    fix: create

--- controllers/GroupMembersController.js
+++ controllers/GroupMembersController.js
@@ -3,10 +3,19 @@ const { createError } = require('../error/error');
 
 exports.create = async (req, res, next) => {
     const { userId, groupId } = req.body
-    let groupMember = await groupMemberService.findByUserIdAndGroupId({ userId, groupId })
+    const { uId } = req.user
+
+    let groupMember = await groupMemberService.findByUserIdAndGroupId({ userId: uId, groupId })
+    if (!groupMember) return next(createError(404, 'You are not a member of this group'))
+
+    groupMember = await groupMemberService.findByUserIdAndGroupId({ userId, groupId })
     if (groupMember) return next(createError(400, 'Member already exists'))
-    groupMember = await groupMemberService.save({ userId, groupId })
-    return res.status(201).json({ message: 'Group member added successfully', groupMember })
+    try {
+        groupMember = await groupMemberService.save({ userId, groupId })
+        return res.status(201).json({ message: 'Group member added successfully', groupMember })
+    } catch (err) {
+        return next(createError(400, 'User does not exists'))
+    }
 }
 
 exports.delete = async (req, res, next) => {
