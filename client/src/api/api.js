cb77fe998b3e2f1bc7a835f223cc7ccb589f6d60
Sreesanth46 <sreesanth46@gmail.com>
2023-06-25 11:31:12 +0530

    fix: post, put, delete requests, getBaseUrl

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const getBaseURL = 'http://localhost:8080'
 
 const axiosParams = {
-    baseURL: getBaseURL(),
+    baseURL: getBaseURL,
     headers: { 'Content-type': 'application/json' },
 }
 
@@ -11,9 +11,9 @@ const axiosInstance = axios.create(axiosParams);
 const api = (axios) => {
     return {
         get: (url, config) => axios.get(url, config),
-        post: (url, config, body) => axios.get(url, body, config),
-        put: (url, config, body) => axios.get(url, body, config),
-        delete: (url, config) => axios.get(url, config)
+        post: (url, body, config) => axios.post(url, body, config),
+        put: (url, body, config) => axios.put(url, body, config),
+        delete: (url, config) => axios.delete(url, config)
     }
 }
 
