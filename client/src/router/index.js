7a37eaddf0e400e09ab82592aa7000bbc6c6c27b
Sreesanth46 <sreesanth46@gmail.com>
2023-06-25 11:33:17 +0530

    feat: routes - register, signup, login

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+const routes = [
+	{
+		path: '/',
+		name: 'Home',
+		component: () => import("@/views/HomeView.vue")
+	},
+
+	{
+		path: '/register',
+		name: 'Register',
+		component: () => import("@/views/Register.vue")
+	},
+
+	{
+		path: '/signup',
+		name: 'Signup',
+		component: () => import("@/views/Signup.vue")
+	},
+
+	{
+		path: '/login',
+		name: 'Login',
+		component: () => import("@/views/Login.vue")
+	},
+]
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: '/',
-			name: 'home',
-			component: HomeView
-		},
-	]
+	routes
 })
 
 export default router
