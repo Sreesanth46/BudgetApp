bb0716a3980f7abb133e570719f0e254879c86a8
Sreesanth46 <sreesanth46@gmail.com>
2023-06-25 18:12:31 +0530

    feat: login api, save access/refresh token

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,3 +1,26 @@
+<script setup>
+import { reactive } from "vue";
+import { login } from "@/api/login.api";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const loginform = reactive({
+    email: "",
+    password: "",
+});
+
+const handleSubmit = async () => {
+    try {
+        const res = await login(loginform);
+        localStorage.setItem("accessToken", res.data.accessToken);
+        localStorage.setItem("refreshToken", res.data.refreshToken);
+        router.push({ name: "Body" });
+    } catch (err) {
+        console.log("Error login in ", err);
+    }
+};
+</script>
+
 <template>
     <section class="bg-gray-50 dark:bg-gray-900">
         <div
@@ -12,7 +35,10 @@
                     >
                         Sign in to your account
                     </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
+                    <form
+                        class="space-y-4 md:space-y-6"
+                        @submit.prevent="handleSubmit()"
+                    >
                         <div>
                             <label
                                 for="email"
@@ -25,7 +51,8 @@
                                 id="email"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="name@company.com"
-                                required=""
+                                v-model="loginform.email"
+                                required
                             />
                         </div>
                         <div>
@@ -38,9 +65,10 @@
                                 type="password"
                                 name="password"
                                 id="password"
-                                placeholder="••••••••"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required=""
+                                placeholder="••••••••"
+                                v-model="loginform.password"
+                                required
                             />
                         </div>
                         <div class="flex items-center justify-between">
@@ -51,7 +79,6 @@
                                         aria-describedby="remember"
                                         type="checkbox"
                                         class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                                        required=""
                                     />
                                 </div>
                                 <div class="ml-3 text-sm">
@@ -62,10 +89,10 @@
                                     >
                                 </div>
                             </div>
-                            <a
-                                href="#"
+                            <router-link
+                                to="#"
                                 class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                                >Forgot password?</a
+                                >Forgot password?</router-link
                             >
                         </div>
                         <button
