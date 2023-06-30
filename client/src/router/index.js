667115c3b3a7c5bd78ddcf57e492a95d4309f64d
Sreesanth46 <sreesanth46@gmail.com>
2023-06-30 12:11:42 +0530

    feat: route guard

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useUserStore } from '../stores/UserStore'
+import { storeToRefs } from 'pinia';
 
 const routes = [
 
@@ -24,13 +26,24 @@ const routes = [
 	{
 		path: '/',
 		name: 'Body',
-		component: () => import("@/views/HomeView.vue")
+		component: () => import("@/views/HomeView.vue"),
+		meta: { Authenticated: true },
+		children: [
+
+		]
 	},
 ]
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes
+})
+
+router.beforeEach(async (to, from) => {
+	const { Authenticated } = to.meta;
+	const userStore = useUserStore();
+	const { getIsLoggedIn } = storeToRefs(userStore)
+	if (Authenticated && !getIsLoggedIn) return { name: 'Login' }
 })
 
 export default router
