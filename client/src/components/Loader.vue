49d776363ea608bd5c941e14e50b5e82fc1a7c10
Sreesanth46 <sreesanth46@gmail.com>
2023-07-03 14:13:15 +0530

    feat: loader compoenet

---
+++ client/src/components/Loader.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted, onUnmounted } from "vue";
+
+onMounted(() => {
+    document.body.classList.add("overflow-hidden");
+});
+
+onUnmounted(() => {
+    document.body.classList.remove("overflow-hidden");
+});
+</script>
+
+<template>
+    <div>
+        <div
+            class="h-screen w-screen fixed top-0 left-0 bg-white z-50 grid place-content-center overflow-hidden"
+        >
+            <img src="@/assets/images/loader.gif" alt="Loading..." />
+        </div>
+    </div>
+</template>
