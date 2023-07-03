6d4f0e68b0ae54e57da352f7915fce98ea94b6ae
Sreesanth46 <sreesanth46@gmail.com>
2023-07-03 14:13:32 +0530

    feat: input component

---
+++ client/src/components/AppInput.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+type TInput = "email" | "password" | "number" | "date" | "search"
+const props = defineProps({
+    name: { type: String, required: true },
+    id: { type: String, required: true },
+    placeholder: { type: String },
+    required: { type: Boolean },
+});
+</script>
+
+<template>
+    <div>
+        <label for="email" class="label-field"><slot></slot></label>
+        <input
+            class="input-field"
+            type="email"
+            :name="name"
+            :id="id"
+            :placeholder="placeholder"
+            :required="required"
+        />
+    </div>
+</template>
+
+<style scoped>
+.input-field {
+    @apply bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500;
+}
+
+.label-field {
+    @apply block mb-2 text-sm font-medium text-gray-900 dark:text-white;
+}
+
+input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+
+input[type="number"] {
+    -moz-appearance: textfield;
+}
+</style>
