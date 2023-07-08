503ba51b6cf57fd15a91e1039d3d47c8ee2b89cf
Sreesanth46 <sreesanth46@gmail.com>
2023-07-08 17:34:02 +0530

    feat: table component

---
+++ client/src/components/AppTable.vue
@@ -0,0 +1,39 @@
+<script setup>
+const props = defineProps(["headers", "data", "keys"]);
+</script>
+
+<template>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table
+            class="w-full text-sm text-left text-gray-500 dark:text-gray-400"
+        >
+            <thead
+                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+            >
+                <tr>
+                    <th
+                        v-for="(header, i) in headers"
+                        :key="i"
+                        scope="col"
+                        class="px-6 py-3"
+                    >
+                        {{ header }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr
+                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                    v-for="(item, i) in data"
+                    :key="i"
+                >
+                    <td v-for="(key, i) in keys" :key="i" class="px-6 py-4">
+                        {{ item[key] }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+// TODO : figure out a way to remove the keys and use values only in both header and data
