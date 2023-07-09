3e8e3ddfd5d5544c66148face47191c3cc940e6e
Sreesanth46 <sreesanth46@gmail.com>
2023-07-09 22:39:54 +0530

    feat: list group

--- client/src/api/group.api.js
+++ client/src/api/group.api.js
@@ -2,6 +2,8 @@ import api from "./api";
 
 export const createGroup = (form) => api.post(`/group`, form);
 
+export const listGroup = () => api.get(`/group`);
+
 export const updateGroup = (form, id) => api.put(`/group/${id}`, form);
 
 export const deleteGroup = (id) => api.delete(`/group/${id}`);
