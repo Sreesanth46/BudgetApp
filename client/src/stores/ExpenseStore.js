14580308996bf714f1c6fe23e6f7b783dece7e3e
Sreesanth46 <sreesanth46@gmail.com>
2023-07-09 23:33:34 +0530

    feat: create, update, delete - expenses

--- client/src/stores/ExpenseStore.js
+++ client/src/stores/ExpenseStore.js
@@ -1,11 +1,13 @@
 import { defineStore } from "pinia";
-import { listExpense } from "@/api/expense.api"
+import { createExpense, listExpense, updateExpense, deleteExpense } from "@/api/expense.api"
+import { STATUSES } from '@/utils/globals'
 
 export const useExpenseStore = defineStore('ExpenseStore', {
     state: () => {
         return {
             error: null,
-            expenses: null
+            expenses: null,
+            status: STATUSES.PENDING
         }
     },
 
@@ -14,11 +16,54 @@ export const useExpenseStore = defineStore('ExpenseStore', {
     actions: {
         async fetchExpense(groupId) {
             try {
+                this.status = STATUSES.LOADING
                 const expenses = await listExpense(groupId)
                 this.expenses = expenses
+                this.status = STATUSES.SUCCESS
+
             } catch (err) {
+                this.status = STATUSES.ERROR
                 this.error = err.response.data.error
             }
-        }
+        },
+
+        async createExpense(form) {
+            try {
+                this.status = STATUSES.LOADING
+                await createExpense(form)
+                await this.fetchExpense()
+                this.status = STATUSES.SUCCESS
+
+            } catch (err) {
+                this.status = STATUSES.ERROR
+                this.error = err.response.data.error
+            }
+        },
+
+        async updateExpense(form, id) {
+            try {
+                this.status = STATUSES.LOADING
+                await updateExpense(form, id)
+                await this.fetchExpense()
+                this.status = STATUSES.SUCCESS
+
+            } catch (err) {
+                this.status = STATUSES.ERROR
+                this.error = err.response.data.error
+            }
+        },
+
+        async deleteExpense(id) {
+            try {
+                this.status = STATUSES.LOADING
+                await listExpense(id)
+                await this.fetchExpense()
+                this.status = STATUSES.SUCCESS
+
+            } catch (err) {
+                this.status = STATUSES.ERROR
+                this.error = err.response.data.error
+            }
+        },
     },
 })
