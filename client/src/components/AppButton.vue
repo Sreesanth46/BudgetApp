d47673e40acd84f8f3d21340bf0865eaf921eb85
Sreesanth46 <sreesanth46@gmail.com>
2023-07-09 23:18:04 +0530

    feat: Button component

---
+++ client/src/components/AppButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+    type: { type: String, default: "submit" },
+});
+</script>
+
+<template>
+    <button
+        :type="type"
+        class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+    >
+        <slot></slot>
+    </button>
+</template>
