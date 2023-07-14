eb818ff6e7ec83086b452ecad2a1fafa368d8a4c
Sreesanth46 <sreesanth46@gmail.com>
2023-07-14 14:07:38 +0530

    refactor: listAllExpense params

--- client/src/api/expense.api.js
+++ client/src/api/expense.api.js
@@ -4,7 +4,7 @@ export const createExpense = (form) => api.post(`/expense`, form);
 
 export const listExpenseByGroup = (groupId) => api.get(`/expense/group/${groupId}`);
 
-export const listAllExpense = (groupId) => api.get(`/expense/all`);
+export const listAllExpense = () => api.get(`/expense/all`);
 
 export const updateExpense = (form, id) => api.put(`/expense/${id}`, form);
 
