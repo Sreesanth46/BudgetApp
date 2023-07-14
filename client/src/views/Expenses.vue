d4e5355bbf77edbbb22f3bf630c17062f5191569
Sreesanth46 <sreesanth46@gmail.com>
2023-07-14 14:09:24 +0530

    feat: expense Table, create expense link

--- client/src/views/Expenses.vue
+++ client/src/views/Expenses.vue
@@ -2,62 +2,36 @@
 import { onMounted, reactive } from "vue";
 import { storeToRefs } from "pinia";
 import { useExpenseStore } from "@/stores/ExpenseStore";
-import AppInput from "@/components/AppInput.vue";
 import AppTable from "@/components/AppTable.vue";
 import Loader from "@/components/Loader.vue";
 import { STATUSES } from "@/utils/globals";
 import AppButton from "@/components/AppButton.vue";
-import { resetForm } from "@/utils/helpers";
 
 const ExpensesStore = useExpenseStore();
+const { getExpenses, getExpenseStatus } = storeToRefs(ExpensesStore);
 
 onMounted(() => {
     ExpensesStore.fetchExpense();
 });
 
-const { getExpenses, getExpenseStatus } = storeToRefs(ExpensesStore);
-
-const expenseForm = reactive({
-    name: "",
-    amount: "",
-    groupId: "",
-});
-
-async function handleSubmit() {
-    await ExpensesStore.createExpense(expenseForm);
-    resetForm(expenseForm);
-}
-
-const keyValue = ["id", "name", "status", "createdAt"];
+const keyValue = ["id", "name", "amount", "status", "createdAt"];
 </script>
 
 <template>
-    <div class="">
-        <h3>Expenses</h3>
-        <div class="my-2 w-1/2">
-            <h4>Create expense</h4>
-            <form
-                @submit.prevent="handleSubmit()"
-                class="space-y-4 md:space-y-4"
+    <div class="p-8">
+        <div class="flex justify-between">
+            <h1
+                class="mb-4 text-lg leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
             >
-                <AppInput
-                    :id="`expense-name-input`"
-                    v-model="expenseForm.name"
-                    :name="`Expense name`"
-                />
-                <AppInput
-                    :id="`expense-amount-input`"
-                    v-model="expenseForm.amount"
-                    :name="`Expense amount`"
-                />
-                <AppInput
-                    :id="`expense-groupId-input`"
-                    v-model="expenseForm.groupId"
-                    :name="`Group ID`"
-                />
-                <AppButton> Create </AppButton>
-            </form>
+                Expense
+            </h1>
+            <div>
+                <router-link :to="{ name: 'CreateExpense' }">
+                    <AppButton>New Expense</AppButton>
+                </router-link>
+            </div>
         </div>
+        <AppTable :headers="keyValue" :keys="keyValue" :data="getExpenses" />
         <Loader v-if="getExpenseStatus === STATUSES.LOADING" />
     </div>
 </template>
