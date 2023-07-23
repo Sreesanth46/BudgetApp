7983e740544ff3d7aa9b07fc1c969983d9c181e9
Sreesanth46 <sreesanth46@gmail.com>
2023-07-23 18:36:48 +0530

    feat: update function

--- client/src/views/CreateExpense.vue
+++ client/src/views/CreateExpense.vue
@@ -1,13 +1,34 @@
 <script setup>
-import { reactive } from "vue";
+import { onMounted, onUnmounted, reactive } from "vue";
 import { resetForm } from "@/utils/helpers";
 import { useExpenseStore } from "@/stores/ExpenseStore";
 import AppInput from "@/components/AppInput.vue";
 import AppButton from "@/components/AppButton.vue";
+import { useRoute, useRouter } from "vue-router";
+import { storeToRefs } from "pinia";
 
+const route = useRoute();
+const router = useRouter();
 const ExpensesStore = useExpenseStore();
+const { getSelectedExpense, getExpenses } = storeToRefs(ExpensesStore);
 
 const props = defineProps(["groupId"]);
+
+onMounted(() => {
+    if (isEditing()) {
+        expenseForm.name = getSelectedExpense.value.name;
+        expenseForm.amount = String(getSelectedExpense.value.amount);
+        expenseForm.groupId = String(getSelectedExpense.value.groupId);
+    }
+});
+
+onUnmounted(() => {
+    ExpensesStore.setSelectedExpense(null);
+});
+
+function isEditing() {
+    return route.name == "Edit Expense";
+}
 
 const expenseForm = reactive({
     name: "",
@@ -16,7 +37,15 @@ const expenseForm = reactive({
 });
 
 async function handleSubmit() {
-    await ExpensesStore.createExpense(expenseForm);
+    if (isEditing()) {
+        await ExpensesStore.updateExpense(
+            expenseForm,
+            getSelectedExpense.value.id
+        );
+    } else {
+        await ExpensesStore.createExpense(expenseForm);
+    }
+    router.push({ name: "Expenses" });
     resetForm(expenseForm);
 }
 </script>
@@ -61,7 +90,9 @@ async function handleSubmit() {
                             Cancel
                         </AppButton>
                     </router-link>
-                    <AppButton> Save </AppButton>
+                    <AppButton>
+                        {{ isEditing() ? "Update" : "Save" }}
+                    </AppButton>
                 </div>
             </div>
         </form>
