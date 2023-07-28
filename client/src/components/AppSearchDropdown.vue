77e6bb1f7761d12d77fa0eac9f38c241dc390b73
Sreesanth46 <sreesanth46@gmail.com>
2023-07-28 11:17:51 +0530

    feat: Search drop down component

---
+++ client/src/components/AppSearchDropdown.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+    placeholder: { type: String },
+    modelValue: { type: String, required: true, default: "" },
+    data: { type: Array, default: [] },
+    name: { type: String },
+    keys: { type: Array, default: [] },
+    id: { type: String },
+});
+
+const isFocused = ref(false);
+
+const reduceObject = (obj, path) => {
+    return path.reduce((prev, key) => {
+        return prev?.[key];
+    }, obj);
+};
+
+const emit = defineEmits(["update:modelValue", "keyup"]);
+
+function updateInput(event) {
+    emit("update:modelValue", event.target.value);
+}
+</script>
+
+<template>
+    <form>
+        <div class="flex flex-col gap-4">
+            <div class="relative w-full">
+                <label
+                    :for="name"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >{{ name }}</label
+                >
+                <input
+                    type="search"
+                    :placeholder="placeholder"
+                    :value="modelValue"
+                    @input="updateInput"
+                    @keyup="() => emit('keyup')"
+                    @focus="() => (isFocused = true)"
+                    @blur="() => (isFocused = false)"
+                    class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                    required
+                />
+            </div>
+            <div
+                id="dropdown"
+                class="w-full z-10 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700"
+            >
+                <ul
+                    class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="dropdown-button"
+                    v-if="data.length > 0 && isFocused"
+                >
+                    <li v-for="item in data" :key="item.id">
+                        <button
+                            type="button"
+                            class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                            <span v-if="keys.length > 1">
+                                {{ reduceObject(item, keys) }}
+                            </span>
+                            <span v-else>{{ item[keys] }}</span>
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </form>
+</template>
