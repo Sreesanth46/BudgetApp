ca8f07e44b6a91531b130ea2481a8620bddd8df8
Sreesanth46 <sreesanth46@gmail.com>
2023-11-25 12:57:32 +0530

    fix: redirect to dashboard if user has access token

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,9 @@
 import { onMounted } from "vue";
 import { storeToRefs } from "pinia";
 import { useUserStore } from "@stores/UserStore";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
 const userStore = useUserStore();
 const { getIsLoggedIn } = storeToRefs(userStore);
 
@@ -10,6 +12,7 @@ onMounted(async () => {
     if (localStorage.getItem("accessToken")) {
         try {
             await userStore.verifyAccessToken();
+            router.push({ name: "Dashboard" });
         } catch (error) {
             localStorage.removeItem("accessToken");
         }
