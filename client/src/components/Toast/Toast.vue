cac55ea9bc075ebe69c4b697c1eea4c6e0fc20c6
Sreesanth46 <sreesanth46@gmail.com>
2023-11-25 14:34:24 +0530

    chore: toast component

--- client/src/components/Toast/Toast.vue
+++ client/src/components/Toast/Toast.vue
@@ -7,64 +7,18 @@ const toastTypes = {
     error: "xt-yellow-800 bg-yellow-50 dark:text-yellow-300",
     default: "text-gray-800 bg-gray-50 dark:text-gray-300",
 };
+const type = toastTypes.info;
 </script>
 
 <template>
     <div class="grid">
         <div
-            class="flex items-center p-4 mb-4 text-sm rounded-lg dark:bg-gray-800 text-blue-800 bg-blue-50 dark:text-blue-400"
+            class="flex items-center p-4 mb-4 text-sm rounded-lg"
             role="alert"
+            :class="type"
         >
             <InfoIcon />
-            <span class="sr-only">Info</span>
-            <div>
-                <span class="font-medium">Info alert!</span> Change a few things
-                up and try submitting again.
-            </div>
-        </div>
-        <div
-            class="flex items-center p-4 mb-4 text-sm rounded-lg dark:bg-gray-800 text-red-800 bg-red-50 dark:text-red-400"
-            role="alert"
-        >
-            <InfoIcon />
-            <span class="sr-only">Info</span>
-            <div>
-                <span class="font-medium">Danger alert!</span> Change a few
-                things up and try submitting again.
-            </div>
-        </div>
-        <div
-            class="flex items-center p-4 mb-4 text-sm rounded-lg dark:bg-gray-800 ext-green-800 bg-green-50 dark:text-green-400"
-            role="alert"
-        >
-            <InfoIcon />
-            <span class="sr-only">Info</span>
-            <div>
-                <span class="font-medium">Success alert!</span> Change a few
-                things up and try submitting again.
-            </div>
-        </div>
-        <div
-            class="flex items-center p-4 mb-4 text-sm rounded-lg dark:bg-gray-800 xt-yellow-800 bg-yellow-50 dark:text-yellow-300"
-            role="alert"
-        >
-            <InfoIcon />
-            <span class="sr-only">Info</span>
-            <div>
-                <span class="font-medium">Warning alert!</span> Change a few
-                things up and try submitting again.
-            </div>
-        </div>
-        <div
-            class="flex items-center p-4 text-sm rounded-lg dark:bg-gray-800 text-gray-800 bg-gray-50 dark:text-gray-300"
-            role="alert"
-        >
-            <InfoIcon />
-            <span class="sr-only">Info</span>
-            <div>
-                <span class="font-medium">Dark alert!</span> Change a few things
-                up and try submitting again.
-            </div>
+            <slot>Change a few things up and try submitting again.</slot>
         </div>
     </div>
 </template>
